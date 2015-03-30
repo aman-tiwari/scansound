713bee5443eb6b4437664c60d4a1ac1b8d5de78d
Aman Tiwari <aman26@gmail.com>
2015-03-31 00:55:17 +0100

    Added commandline arguments
    now you can call `python scansound.py <image-to-scan> <audio-output-filename>'`

--- scansound.py
+++ scansound.py
@@ -4,14 +4,17 @@ from scipy import misc
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 from instuments import piano_freqs
-#
-#   low
-#   mid
-#   high
-#
-#
 
-in_image = misc.imread('x.png')
+import sys
+
+if len(sys.argv) != 2:
+    print 'Usage: python scansound.py <image-to-scan> <audio-output-filename>'
+    raise SystemExit
+
+img_file = sys.argv[0]
+audio_file = sys.argv[1]
+
+in_image = misc.imread(img_file)
 np.set_printoptions(linewidth = 300)
 n_cols = len(in_image[0,:])
 n_rows = len(in_image[:,0])
@@ -25,14 +28,11 @@ t = np.arange(0.0, float(n_cols)/cols_per_sec, dt)
 
 sin_array = []
 
-#logspace: np.logspace(np.log10(30),np.log10(20000), 64)
-for freq in piano_freqs: #logspace generates an array of evenly spaced numbers, 64 between 30 &
+#logspace: np.logspace(np.log10(30),np.log10(20000), 64) ~ can use instead of piano freqs for weird sound
+for freq in piano_freqs: 
     sin_array.append(np.sin(2*pi*freq*t))
 
 sin_array = np.asarray(sin_array)
-
-print len(sin_array)
-
 
 print 'Cols: ' + str(n_cols)
 print 'Rows: ' + str(n_rows)
@@ -47,7 +47,7 @@ for i in range(n_rows):
 
 summed_array = np.sum(sin_array, axis=0)
 summed_array /= np.max(np.abs(summed_array),axis=0)
-wavfile.write('t.wav', rate=44000, data=summed_array.astype('float32'))
+wavfile.write(audio_file, rate=44000, data=summed_array.astype('float32'))
 
 plt.specgram(summed_array.astype('float32'), Fs=44000)
 plt.show()
